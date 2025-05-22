c1a158841275b1b859c5f8f56bfb8a2fa70ff9a3
Aliyu Idris <drisatech@gmail.com>
2025-05-22 23:42:35 +0100

    Update Streamlit-Api.py

--- Streamlit-Api.py
+++ Streamlit-Api.py
@@ -7,7 +7,7 @@ from PIL import Image
 
 # Display DRISA logo
 logo = Image.open("Drisa_Logo.jpg")
-st.image(logo, use_column_width=False, width=150)
+st.image(logo, use_container_width=True)
 
 st.markdown("<div style='text-align: center;'><img src='Drisa_Logo.jpg' width='150'></div>", unsafe_allow_html=True)
 
